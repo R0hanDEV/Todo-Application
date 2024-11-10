5a974c5b969444f0a77b8aa396e25a5b8942b0e3
rohan <r0han45dev@gmail.com>
2024-11-10 08:27:32 +0530

    todo app with edit delete and add done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,95 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React, { useState } from "react";
+import "./App.css";
 function App() {
-  const [count, setCount] = useState(0)
+  const [todoList, setTodoList] = useState([]);
+  const [todoText, setTodoText] = useState("");
+
+  const handleAddTodo = () => {
+    if (todoText != "") {
+      setTodoList((prev) => [
+        ...prev,
+        {
+          id: Math.random(),
+          todoname: todoText.trim(),
+        },
+      ]);
+    }
+  };
+
+  const handleDelete = (id) => {
+    setTodoList((prev) => prev.filter((item) => item.id != id.target.value));
+  };
+
+  const handleEdit = (e) => {
+    const idToEdit = Number(e.target.getAttribute("data-attribute"));
+
+    const updatedText = e.target.value;
+
+    setTodoList((prev) =>
+      prev.map((todo) =>
+        todo.id === idToEdit ? { ...todo, todoname: updatedText } : todo
+      )
+    );
+  };
+
+  const onInputChange = (e) => {
+    setTodoText(e.target.value);
+  };
+
+  const renderTodo = (item) => {
+    return (
+      <div className="todo-item">
+        <input
+          key={item.id} // Always provide a unique key when mapping
+          value={item.todoname} // Bind the input to the current item's name
+          name="todo"
+          onChange={handleEdit} // Attach the handler to update state on change
+          data-attribute={item.id} // Pass the item's ID for identification
+        />
+        <button onClick={handleDelete} value={item.id}>
+          delete
+        </button>
+      </div>
+    );
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app-section ">
+        <h1>Todo Application</h1>
+
+        <div className="todo-section">
+          <div className="input-Add-todo">
+            <input
+              name="todo"
+              className="todo "
+              value={todoText}
+              onChange={(e) => {
+                onInputChange(e);
+              }}
+            />
+            <button onClick={handleAddTodo}>Add</button>
+          </div>
+
+          <div className="wrapper">
+            {todoList &&
+              todoList.map((item, index) => {
+                return (
+                  <React.Fragment key={index}>
+                    {renderTodo(item)}
+                  </React.Fragment>
+                );
+              })}
+          </div>
+          {todoList.length === 0 && (
+            <>
+              <div className="no-todo">No todo found</div>
+            </>
+          )}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
